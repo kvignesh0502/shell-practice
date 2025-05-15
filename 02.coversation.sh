377a044b127a353d81fa9749672435864379890e
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-15 15:59:47 +0100

    inside server

--- 02.coversation.sh
+++ 02.coversation.sh
@@ -3,8 +3,8 @@
 #shebang is the first line of shell script
 # (#) ->> This is comment
 
-PERSON1=vignesh
-PERSON2=abhi
+PERSON1=vignesh #In this we have learned to assign a value to variable (key = value)
+PERSON2=abhi # to call the variable we need to use ($)
 
 echo $PERSON1 : " Hi $PERSON2, how are you ? "
 echo $PERSON2 : " I am good and you."
