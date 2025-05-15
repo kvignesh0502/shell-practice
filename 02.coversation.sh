14a815c3054c9d5cdc715ec209c2c003f7b3e5e3
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-15 15:55:18 +0100

    inside server

---
+++ 02.coversation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#shebang is the first line of shell script
+# (#) ->> This is comment
+
+PERSON1=vignesh
+PERSON2=abhi
+
+echo "vignesh : " Hi Abhi, how are you ? "
+echo "abhi : " I am good and you."
+echo "vignesh : what's plans tonight."
+echo "abhi : nothing much."
