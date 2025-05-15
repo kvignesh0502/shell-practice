e7cd982670346283c81480ad8785e3386b568074
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-15 15:56:33 +0100

    inside server

--- 02.coversation.sh
+++ 02.coversation.sh
@@ -6,7 +6,7 @@
 PERSON1=vignesh
 PERSON2=abhi
 
-echo "vignesh : " Hi Abhi, how are you ? "
-echo "abhi : " I am good and you."
-echo "vignesh : what's plans tonight."
-echo "abhi : nothing much."
+echo vignesh : " Hi Abhi, how are you ? "
+echo abhi : " I am good and you."
+echo vignesh : "what's plans tonight."
+echo abhi : "nothing much."
