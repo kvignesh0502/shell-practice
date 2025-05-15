5a0afa453a8bb66bc8a6173b4a471003ec8d6b6d
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-15 16:01:29 +0100

    inside server

---
+++ 03.variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#shebang is the first line of shell script
+# (#) ->> This is comment
+
+PERSON1=vignesh #In this we have learned to assign a value to variable (key = value)
+PERSON2=abhi # to call the variable we need to use ($)
+
+echo $PERSON1 : " Hi $PERSON2, how are you ? "
+echo $PERSON2 : " I am good and you."
+echo $PERSON1 : "what's plans tonight."
+echo $PERSON2 : "nothing much."
