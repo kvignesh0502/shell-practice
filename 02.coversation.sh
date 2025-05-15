0ccc9dbf3084640000959570d5d9537d1e06a97e
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-15 16:39:53 +0100

    shell-practice

--- 02.coversation.sh
+++ 02.coversation.sh
@@ -3,7 +3,7 @@
 #shebang is the first line of shell script
 # (#) ->> This is comment
 
-echo $PERSON1 : " Hi $PERSON2, how are you ? "
-echo $PERSON2 : " I am good and you."
-echo $PERSON1 : "what's plans tonight."
-echo $PERSON2 : "nothing much."
+echo $PERSON1 :: " Hi $PERSON2, how are you ? "
+echo $PERSON2 :: " I am good and you."
+echo $PERSON1 :: "what's plans tonight."
+echo $PERSON2 ::"nothing much."
