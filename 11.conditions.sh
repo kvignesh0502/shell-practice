a4f543ff87fa2eae26c0fdb6ff5d4af5485efa58
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-16 22:16:42 +0100

    special vars

--- 11.conditions.sh
+++ 11.conditions.sh
@@ -2,6 +2,13 @@
 
 # Write a Shell script to check if given number is greater than 50 ?
 
+#if condition syntax
+#if [ expression ]
+#    Statement if expression is true
+#else 
+#   Statement if expression is flase
+#fi
+
 NUMBER=$1
 
 if [ $NUMBER -gt 50 ]
@@ -10,3 +17,9 @@ then
 else 
     echo "Given number $NUMBER is less than 50"
 fi
+
+# Operators in Shell
+# -gt --> greater than
+# -lt --> less than
+# -eq --> equal
+# -ne --> not equal
