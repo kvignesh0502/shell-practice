ea70247f3e47af32701fb8457747e412aa9436e4
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-19 23:48:29 +0100

    Colors

--- 14.colors.sh
+++ 14.colors.sh
@@ -5,9 +5,9 @@
 USERID=$(id -u)
 if [ $USERID -eq 0 ]
 then
-    echo "User is root...$G Proceed $N"
+    echo -e "User is root...$G Proceed $N"
 else
-    echo "$R ERROR : Run with Root $N "
+    echo -e "$R ERROR : Run with Root $N "
     exit 1
 fi
 
@@ -21,9 +21,9 @@ N="\e[0m"
 VALIDATE(){                                  
 if [ $1 -eq 0 ]                            
 then
-    echo "Installing $2...$G SUCCESS $N"         
+    echo -e "Installing $2...$G SUCCESS $N"         
 else
-    echo "INSTALLATION $2..$R FAILED $N"         
+    echo -e "INSTALLATION $2..$R FAILED $N"         
     exit 1                                   
 fi
 }
@@ -31,7 +31,7 @@ fi
 dnf list installed mysql
 if [ $? -eq 0 ]
 then
-    echo "Nothing to do...$Y mysql installed $N"
+    echo -e "Nothing to do...$Y mysql installed $N"
 else
     echo "NOT installed.. Installing Now"
     dnf install mysql -y
@@ -41,7 +41,7 @@ fi
 dnf list installed nginx 
 if [ $? -eq 0 ]
 then
-    echo "Nothing to do... $Y Nginx installed $N"
+    echo -e "Nothing to do... $Y Nginx installed $N"
 else
     echo "NOT installed.. Nginx Installing Now"
     dnf install nginx -y
