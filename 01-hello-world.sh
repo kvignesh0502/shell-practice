9b04e485218462f4515fee242aeb7d7352221a87
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-20 00:35:13 +0100

    Colors

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -3,4 +3,4 @@
 echo " This is first shell script" 
 echo "hello Vignesh, Now you are in Linux server"
 
-#Command t o run the script: sh 01.hello_world.sh
+#Command to run the script: sh 01.hello_world.sh
--- 14-colors.sh
+++ 14-colors.sh
@@ -2,48 +2,53 @@
 
 # we are going to install mqsql nginx
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+R="\e[31m"      # Red
+G="\e[32m"      # Green
+Y="\e[33m"      # Yellow
+B="\e[34m"      # Blue
+M="\e[35m"      # Magenta
+C="\e[36m"      # Cyan
+W="\e[37m"      # White
+K="\e[30m"      # Black
+N="\e[0m"       # Reset
 
-echo "Script started Executing at : $(date)" 
+echo "Script started Executing at : $(date)"  # Print current date and time
 
-USERID=$(id -u)
-if [ $USERID -eq 0 ]
+USERID=$(id -u)  # Get current user ID
+if [ $USERID -eq 0 ]  # Check if user is root
 then
-    echo -e "User is root...$G Proceed $N" 
+    echo -e "User is root...$G Proceed $N"  # If root, proceed
 else
-    echo -e "$R ERROR : Run with Root $N " 
-    exit 1
+    echo -e "$R ERROR : Run with Root $N "  # If not root, show error
+    exit 1  # Exit script
 fi
 
-VALIDATE(){                                  
-if [ $1 -eq 0 ]                            
+VALIDATE(){                                  # Function to validate installation
+if [ $1 -eq 0 ]                              # Check if exit status is 0
 then
-    echo -e "Installing $2...$G SUCCESS $N"         
+    echo -e "Installing $2...$G SUCCESS $N"  # If success, print success message
 else
-    echo -e "INSTALLATION $2..$R FAILED $N"         
-    exit 1                                   
+    echo -e "INSTALLATION $2..$R FAILED $N"  # If failed, print failure message
+    exit 1                                   # Exit script
 fi
 }
 
-dnf list installed mysql 
-if [ $? -eq 0 ]
+dnf list installed mysql  # Check if mysql is installed
+if [ $? -eq 0 ]  # If installed
 then
-    echo -e "Nothing to do...$Y mysql installed $N" 
+    echo -e "Nothing to do...$Y mysql installed $N"  # Print already installed message
 else
-    echo "NOT installed.. Installing Now" 
-    dnf install mysql -y
-    VALIDATE $? mysql
+    echo "NOT installed.. Installing Now"  # Print installing message
+    dnf install mysql -y  # Install mysql
+    VALIDATE $? mysql  # Validate installation
 fi
 
-dnf list installed nginx 
-if [ $? -eq 0 ]
+dnf list installed nginx  # Check if nginx is installed
+if [ $? -eq 0 ]  # If installed
 then
-    echo -e "Nothing to do... $Y Nginx installed $N" 
+    echo -e "Nothing to do... $Y Nginx installed $N"  # Print already installed message
 else
-    echo "NOT installed.. Nginx Installing Now" 
-    dnf install nginx -y
-    VALIDATE $? Nginx
+    echo "NOT installed.. Nginx Installing Now"  # Print installing message
+    dnf install nginx -y  # Install nginx
+    VALIDATE $? Nginx  # Validate installation
 fi
