53b304ca0307eca905845c0382c55a253c39e7fe
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-20 00:36:14 +0100

    Colors

--- 15-logs.sh
+++ 15-logs.sh
@@ -2,61 +2,6 @@
 
 # we are going to install mqsql nginx
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/logs/shellscript-logs"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
-
-mkdir -p $LOGS_FOLDER
-echo "Script started Executing at : $(date)" | tee -a $LOG_FILE
-
-USERID=$(id -u)
-if [ $USERID -eq 0 ]
-then
-    echo -e "User is root...$G Proceed $N" | tee -a $LOG_FILE
-else
-    echo -e "$R ERROR : Run with Root $N " | tee -a $LOG_FILE
-    exit 1
-fi
-
-VALIDATE(){                                  
-if [ $1 -eq 0 ]                            
-then
-    echo -e "Installing $2...$G SUCCESS $N" | tee -a $LOG_FILE        
-else
-    echo -e "INSTALLATION $2..$R FAILED $N" | tee -a $LOG_FILE       
-    exit 1                                   
-fi
-}
-
-dnf list installed mysql &>>$LOG_FILE
-if [ $? -eq 0 ]
-then
-    echo -e "Nothing to do...$Y mysql installed $N" | tee -a $LOG_FILE
-else
-    echo "NOT installed.. Installing Now" | tee -a $LOG_FILE
-    dnf install mysql -y
-    VALIDATE $? mysql
-fi
-
-dnf list installed nginx &>>$LOG_FILE
-if [ $? -eq 0 ]
-then
-    echo -e "Nothing to do... $Y Nginx installed $N" | tee -a $LOG_FILE
-else
-    echo "NOT installed.. Nginx Installing Now" | tee -a $LOG_FILE
-    dnf install nginx -y
-    VALIDATE $? Nginx
-fi
-
-#!/bin/bash
-
-# we are going to install mqsql nginx
-
 R="\e[31m"      # Red
 G="\e[32m"      # Green
 Y="\e[33m"      # Yellow
@@ -102,5 +47,3 @@ else
     dnf install mysql -y &>>$LOG_FILE                                   # Install MySQL and log
     VALIDATE $? mysql                                                   # Validate installation
 fi
-
-dnf
