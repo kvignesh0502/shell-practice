1e13dd042970fe50ff3b709d2e3d0398c50822ad
kvignesh0502 <vkattamudi88@gmail.com>
2025-05-20 00:24:51 +0100

    Colors

--- 15-logs.sh
+++ 15-logs.sh
@@ -12,23 +12,23 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
 
 mkdir -p $LOGS_FOLDER
-echo "Script started Executing at : $(date)" &>>$LOG_FILE
+echo "Script started Executing at : $(date)" | tee -a $LOG_FILE
 
 USERID=$(id -u)
 if [ $USERID -eq 0 ]
 then
-    echo -e "User is root...$G Proceed $N" &>>$LOG_FILE
+    echo -e "User is root...$G Proceed $N" | tee -a $LOG_FILE
 else
-    echo -e "$R ERROR : Run with Root $N " &>>$LOG_FILE
+    echo -e "$R ERROR : Run with Root $N " | tee -a $LOG_FILE
     exit 1
 fi
 
 VALIDATE(){                                  
 if [ $1 -eq 0 ]                            
 then
-    echo -e "Installing $2...$G SUCCESS $N" &>>$LOG_FILE        
+    echo -e "Installing $2...$G SUCCESS $N" | tee -a $LOG_FILE        
 else
-    echo -e "INSTALLATION $2..$R FAILED $N" &>>$LOG_FILE        
+    echo -e "INSTALLATION $2..$R FAILED $N" | tee -a $LOG_FILE       
     exit 1                                   
 fi
 }
@@ -36,9 +36,9 @@ fi
 dnf list installed mysql &>>$LOG_FILE
 if [ $? -eq 0 ]
 then
-    echo -e "Nothing to do...$Y mysql installed $N" &>>$LOG_FILE
+    echo -e "Nothing to do...$Y mysql installed $N" | tee -a $LOG_FILE
 else
-    echo "NOT installed.. Installing Now" &>>$LOG_FILE
+    echo "NOT installed.. Installing Now" | tee -a $LOG_FILE
     dnf install mysql -y
     VALIDATE $? mysql
 fi
@@ -46,9 +46,9 @@ fi
 dnf list installed nginx &>>$LOG_FILE
 if [ $? -eq 0 ]
 then
-    echo -e "Nothing to do... $Y Nginx installed $N" &>>$LOG_FILE
+    echo -e "Nothing to do... $Y Nginx installed $N" | tee -a $LOG_FILE
 else
-    echo "NOT installed.. Nginx Installing Now" &>>$LOG_FILE
+    echo "NOT installed.. Nginx Installing Now" | tee -a $LOG_FILE
     dnf install nginx -y
     VALIDATE $? Nginx
 fi
